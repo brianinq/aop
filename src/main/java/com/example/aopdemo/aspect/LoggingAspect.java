dd54336398b0103410a572a248bc63cc00a58846
Brian Kipkoech <kipkoechbrian0@gmail.com>
2024-02-27 13:10:38 +0300

    using wildcards on pointcut expression

--- src/main/java/com/example/aopdemo/aspect/LoggingAspect.java
+++ src/main/java/com/example/aopdemo/aspect/LoggingAspect.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-    @Before("execution( * addAccount())")
+    @Before("execution( * add*())")
     public void logBeforeAdvice(){
         System.out.println("Logging before method call");
     }
