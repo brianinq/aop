daea1f82fa4711c838ff4ecd04547cb3da361652
Brian Kipkoech <kipkoechbrian0@gmail.com>
2024-02-27 13:36:44 +0300

    param wildcards

--- src/main/java/com/example/aopdemo/aspect/LoggingAspect.java
+++ src/main/java/com/example/aopdemo/aspect/LoggingAspect.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class LoggingAspect {
-    @Before("execution( * add*())")
+//    @Before("execution( * add*())") // no args
+//    @Before("execution( * add*(*))") // method with one arg of any type
+    @Before("execution( * addAccount(..))")  // 0 or more arguments of any type
     public void logBeforeAdvice(){
         System.out.println("Logging before method call");
     }
